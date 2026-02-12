41a832d061202ad8f2f53dc77ba8489d5c393ca3
timeismoneyinc2019-crypto <timeismoneyinc2019@gmail.com>
2026-02-12 01:49:24 -0600

    Update reputation.py

--- src/nexus/reputation.py
+++ src/nexus/reputation.py
@@ -1,49 +1,49 @@
+import hashlib
+
 class ReputationManager:
     def __init__(self):
-        self.reputations = {}
+        self.reputations = {}  # user_id: reputation_score
+        self.anchor_log = {}   # user_id: list of anchor_ids for audit
 
     def get_reputation(self, user_id):
         return self.reputations.get(user_id, 0)
 
-    def update_reputation(self, user_id, amount):
+    def update_reputation(self, user_id, amount, anchor_id=None, thermodynamic_cost=None):
+        """
+        Update reputation with optional entropic anchor data.
+        Reputation change can be weighted by thermodynamic_cost.
+        """
+        weight = thermodynamic_cost if thermodynamic_cost is not None else 1.0
+        adjusted_amount = amount * weight
+
         if user_id in self.reputations:
-            self.reputations[user_id] += amount
+            self.reputations[user_id] += adjusted_amount
         else:
-            self.reputations[user_id] = amount
+            self.reputations[user_id] = adjusted_amount
+
+        if anchor_id:
+            self.anchor_log.setdefault(user_id, []).append(anchor_id)
+
+    def verify_anchor_integrity(self, user_id, anchor_id):
+        """
+        Verify if the anchor_id exists in the user's anchor log.
+        """
+        return anchor_id in self.anchor_log.get(user_id, [])
 
 class SlashingManager:
     def __init__(self):
-        self.penalties = {}
+        self.penalties = {}  # user_id: penalty_score
 
-    def apply_slashing(self, user_id, penalty):
+    def apply_slashing(self, user_id, penalty, entropy_validation_passed=True):
+        """
+        Apply slashing penalty. If entropy validation fails, increase penalty.
+        """
+        adjusted_penalty = penalty
+        if not entropy_validation_passed:
+            # Increase penalty by 50% if entropy proof fails
+            adjusted_penalty *= 1.5
+
         if user_id in self.penalties:
-            self.penalties[user_id] += penalty
+            self.penalties[user_id] += adjusted_penalty
         else:
-            self.penalties[user_id] = penalty
-
-    def get_penalty(self, user_id):
-        return self.penalties.get(user_id, 0)
-
-class StakeManager:
-    def __init__(self):
-        self.stakes = {}
-
-    def stake(self, user_id, amount):
-        if user_id in self.stakes:
-            self.stakes[user_id] += amount
-        else:
-            self.stakes[user_id] = amount
-
-    def get_stake(self, user_id):
-        return self.stakes.get(user_id, 0)
-
-class GameTheorySequencer:
-    def __init__(self):
-        self.actions = []
-
-    def add_action(self, action):
-        self.actions.append(action)
-
-    def execute_actions(self):
-        for action in self.actions:
-            action.execute()  
+            self.penalties[user_id] = adjusted_penalty
